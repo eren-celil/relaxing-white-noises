2685b0cd27e245605ff917953e9d839e6deaf3ea
Celil6p <vublayancanavar@gmail.com>
2022-10-06 22:33:00 +0300

    Audio stops when other card clicked

--- js.js
+++ js.js
@@ -3,6 +3,7 @@
 const audioChill = new Audio("./chill.mp3");
 const audioStudy = new Audio("./study.mp3");
 const audioSleep = new Audio("./rain.mp3");
+const allAudioStop = document.querySelectorAll('audio').forEach(el => el.pause());
 
 
 function playAudio(audio){
@@ -14,14 +15,23 @@ function playAudio(audio){
     audio.pause();}
     }
     
-    // activating focus effect
+    
   
-function stopAudio(){
-    audioChill.pause();
-    audioStudy.pause();
-    audioSleep.pause();
+function stopAudio(audio){
+    if (audio == audioChill){
+        audioStudy.pause();
+        audioSleep.pause();
+    }
+    if (audio == audioStudy){
+        audioChill.pause();
+        audioSleep.pause();
+    }
+    if (audio == audioSleep){
+        audioStudy.pause();
+        audioChill.pause();
+    }
 }
-   
+   // activating focus effect
    
     const cards = document.querySelectorAll('.voiceCard');
 
