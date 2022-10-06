717c74dcbc3bdbd375cc11870d40df6ccc062a2f
Celil6p <vublayancanavar@gmail.com>
2022-10-06 22:01:59 +0300

    all audios has been added

--- js.js
+++ js.js
@@ -1,11 +1,12 @@
     // play stop audio
 
-const audio = new Audio("./chill.mp3")
+const audioChill = new Audio("./chill.mp3");
+const audioStudy = new Audio("./study.mp3");
+const audioSleep = new Audio("./rain.mp3");
 
-function playAudioChill(){
-    
-    
-   audio.loop = true;
+
+function playAudio(audio){
+    audio.loop = true;
     if(audio.paused){
         audio.play();
     }
@@ -15,7 +16,11 @@ function playAudioChill(){
     
     // activating focus effect
   
-   
+function stopAudio(){
+    audioChill.pause();
+    audioStudy.pause();
+    audioSleep.pause();
+}
    
    
     const cards = document.querySelectorAll('.voiceCard');
